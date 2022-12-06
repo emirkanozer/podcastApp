88bb1603b81dd72e1b1a5898979ec4bd7e319608
emirkanozer <emirkan.ozer@outlook.com>
2022-12-07 01:26:32 +0300

    feat: added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { IPodcast } from './interfaces/podcasts';
+import { HttpService } from './services/http.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'podcastApp';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpService) {
+    this.http.getPodcasts();
+    this.podcast=this.http.podcasts;
+  }
+  podcast:IPodcast[];
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PodcastComponent } from './components/podcast/podcast.component';
+import { PodcastDetailComponent } from './components/podcast-detail/podcast-detail.component';
+import { PlayerComponent } from './components/player/player.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    SearchBarComponent,
+    PodcastComponent,
+    PodcastDetailComponent,
+    PlayerComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
