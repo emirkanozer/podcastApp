e4b8deefe211fda14a79942d37c602bec8efd6d5
emirkanozer <emirkan.ozer@outlook.com>
2022-12-08 01:04:01 +0300

    feat : search bar component add

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch, faU } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  faSearch=faSearch;
+  faUser=faUser;
 }
