11e0a982215ded852082997ba6ae29983cef2a58
emirkanozer <emirkan.ozer@outlook.com>
2022-12-09 03:18:21 +0300

    feat: setted router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PodcastDetailComponent } from './components/podcast-detail/podcast-detail.component';
+import { PodcastComponent } from './components/podcast/podcast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PodcastComponent,
+},
+{
+  path: 'detail',
+  component: PodcastDetailComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
