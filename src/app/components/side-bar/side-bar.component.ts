867d7701dc77f5981f1e5c540e6514b0235e0e1d
emirkanozer <emirkan.ozer@outlook.com>
2022-12-09 03:20:42 +0300

    feat: setted sidebar component

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,6 +1,7 @@
 import { IPodcast } from './../../interfaces/podcasts';
 import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
+import { faHome, faSearch,faHeart,faArrowRight,faHeadphones,faUser,faPodcast,faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-bar',
@@ -8,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-
+  sideClass:boolean=true;
+  faHome = faHome;
+  faSearch = faSearch;
+  faHeart = faHeart;
+  faPodcast = faPodcast;
+  faArrowLeft=faArrowLeft;
+  faUser = faUser;
+  faArrowRight = faArrowRight;
   constructor(private http:HttpService) {
   }
 
   podcast!:IPodcast;
-  async ngOnInit() {
-    this.podcast = await this.http.getPodcasts();
-  }
+   ngOnInit() {
+     //this.podcast = await this.http.getPodcasts();
+    }
 
+  sideBar(){
+    return console.log(this.sideClass = !this.sideClass)
+  }
 }
