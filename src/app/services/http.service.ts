f5e2eb51dbfd2a2911e04db82b208aaf06c4bb56
emirkanozer <emirkan.ozer@outlook.com>
2022-12-09 05:18:39 +0300

    feat: added data

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -29,9 +29,31 @@ export class HttpService {
         const updatedValue = [...currentValue, res];
         this.podcasts.next(updatedValue)
       })
+      Parse('http://api.sr.se/api/rss/pod/10468').then(res => {
+        const currentValue = this.podcasts.value;
+        const updatedValue = [...currentValue, res];
+        this.podcasts.next(updatedValue)
+      })
+      Parse('http://api.sr.se/api/rss/pod/10469').then(res => {
+        const currentValue = this.podcasts.value;
+        const updatedValue = [...currentValue, res];
+        this.podcasts.next(updatedValue)
+      })
 
-
-
+      for (let index = 4000; index < 4024; index++) {
+        Parse('http://api.sr.se/api/rss/pod/'+index).then(res => {
+        const currentValue = this.podcasts.value;
+        const updatedValue = [...currentValue, res];
+        this.podcasts.next(updatedValue)
+      })
+      }
+      for (let index = 3961; index < 3968; index++) {
+        Parse('http://api.sr.se/api/rss/pod/'+index).then(res => {
+        const currentValue = this.podcasts.value;
+        const updatedValue = [...currentValue, res];
+        this.podcasts.next(updatedValue)
+      })
+    }
   }
 }
 
